9c920b786e067d4cf211af365a58e1019c8e6fb1
JoLu11 <113925771+JoLu11@users.noreply.github.com>
2022-10-18 14:54:45 -0400

    Added UI Based off new global battery variable

---
+++ Assets/Scripts/AbilitySystem/Battery.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battery : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int currentBat;
+
+    [SerializeField]
+    [Tooltip("Maximum Battery Amount")]
+    public int maxBat;
+
+    Battery(int max)
+    {
+        this.currentBat = max;
+        this.maxBat = max;
+    }
+
+    public void refill()
+    {
+        this.currentBat = this.maxBat;
+    }
+
+    public void addToCurrent(int amount)
+    {
+        this.currentBat += amount;
+    }
+
+    public void subFromCurrent(int amount)
+    {
+        this.currentBat -= amount;
+    }
+    public bool hasLeft(int amount)
+    {
+        return this.currentBat >= amount;
+    }
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

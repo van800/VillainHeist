02b98ba23c08138367acdbcb6f98165256df5d3f
SpencerSoodek <spencersoodek@gmail.com>
2022-10-19 19:14:34 -0400

    Recharge Station Script added

--- Assets/Scripts/AbilitySystem/AbilityInputs.cs
+++ Assets/Scripts/AbilitySystem/AbilityInputs.cs
@@ -154,6 +154,7 @@ public class AbilityInputs : MonoBehaviour
         }
     }
 
+
     private IEnumerator ShootableRoutine()
     {
         yield return new WaitUntil(() => Input.GetButtonUp(buttonToSelect));
@@ -171,6 +172,10 @@ public class AbilityInputs : MonoBehaviour
             AbilitySystemEnd();
         }
     }
+
+	public void refill() {
+		this.bat.currentBat = this.bat.maxBat;
+	}
 
     private void ApplyAbility()
     {

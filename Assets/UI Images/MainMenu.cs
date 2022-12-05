a6b3b66ce8fafd2eb365459300a469c2430dfb3d
Luke Taylor <luke@bigring.org>
2022-12-05 01:44:46 -0500

    UI Updates

--- Assets/UI Images/MainMenu.cs
+++ Assets/UI Images/MainMenu.cs
@@ -11,26 +11,43 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        UnityEngine.Cursor.lockState = CursorLockMode.None;
         _uiDocument = GetComponent<UIDocument>();
         _uiDocument.rootVisualElement.Q<Button>("Play").clicked += () => Play();
         _uiDocument.rootVisualElement.Q<Button>("Credits").clicked += () => Credits();
         _uiDocument.rootVisualElement.Q<Button>("AssetShowcase").clicked += () => AssetShowcase();
         _uiDocument.rootVisualElement.Q<Button>("Quit").clicked += () => Quit();
+        _uiDocument.rootVisualElement.Q<Button>("Done").clicked += () =>
+        {
+            _uiDocument.rootVisualElement.Q<VisualElement>("DimZone").AddToClassList("hidden");
+            _uiDocument.rootVisualElement.Q<VisualElement>("DimZone").RemoveFromClassList("show");
+            _uiDocument.rootVisualElement.Q<VisualElement>("CreditsBG").AddToClassList("hidden");
+            _uiDocument.rootVisualElement.Q<VisualElement>("CreditsBG").RemoveFromClassList("show");
+        };
     }
 
     private void Play()
     {
+        UnityEngine.Cursor.lockState = CursorLockMode.Locked;
         SceneManager.LoadScene("Tutorial");
         print("PLAY");
+        // Credits DimZone Done
     }
     
     private void Credits()
     {
         print("CREDITS");
+        _uiDocument.rootVisualElement.Q<VisualElement>("DimZone").RemoveFromClassList("hidden");
+        _uiDocument.rootVisualElement.Q<VisualElement>("DimZone").AddToClassList("show");
+        _uiDocument.rootVisualElement.Q<VisualElement>("CreditsBG").RemoveFromClassList("hidden");
+        _uiDocument.rootVisualElement.Q<VisualElement>("CreditsBG").AddToClassList("show");
     }
+    
+    
     
     private void AssetShowcase()
     {
+        UnityEngine.Cursor.lockState = CursorLockMode.Locked;
         SceneManager.LoadScene("AssetShowcase");
         print("SHOWCASE");
     }
